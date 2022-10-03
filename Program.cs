e16e0de6af9b63b7fcaa945b619d606d2e868d5c
Burkov Evgenii <Burkovevgeniy@yansex.ru>
2022-10-03 16:59:42 +0300

    dz

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿//DZ 64
+
+Console.Write("Введи N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
+
+
+//DZ 66
+
+int InputNumber(string input)
+{
+     Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+ }
+
+ void PrintSum(int m, int n, int sum)
+ {
+     sum = sum + n;
+     if (n <= m)
+    {
+         Console.Write($" {sum} ");
+         return;
+     }
+     PrintSum(m, n - 1, sum);
+
+ }
+ int m = InputNumber("Ввод m: ");
+ int n = InputNumber("Ввод n: ");
+ int temp = m;
+
+ if (m > n)
+ {
+     m = n;
+     n = temp;
+ }
+ Console.Write($"{m}, {n} ->");
+ PrintSum(m, n, temp = 0);
+
+//DZ68
+
+int InputNumber(string input)
+{
+Console.Write(input);
+int output = Convert.ToInt32(Console.ReadLine());
+return output;
+}
+int Akkerman(int m, int n)
+{
+if (m == 0)
+{
+return n + 1;
+}
+else if (n == 0 && m > 0)
+{
+return Akkerman(m - 1, 1);
+}
+else
+{
+return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+}
+
+int m = InputNumber("Ввод m: ");
+int n = InputNumber("Ввод n: ");
+int akkermanFunction = Akkerman(m, n);
+Console.Write($"m = {m}, n = {n} - > A(m,n) = {akkermanFunction} ");
